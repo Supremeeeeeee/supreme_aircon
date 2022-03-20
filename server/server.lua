01c775a451bf4aa7de5c6b55fc5e0059eaa57ffb
DuncanEll <59232670+Supremeeeeeee@users.noreply.github.com>
2022-03-20 01:07:22 +0000

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,13 +1,13 @@
 local ESX = nil
 
 local airconItems = { 
-    [1] = {chance = -1, id = 'scrappart', name = 'AC Unit Part', quantity = math.random(10,50), limit = 500},
+    [1] = {chance = -1, id = 'scrapmetal', name = 'AC Unit Part', quantity = math.random(10,50), limit = 500},
     [2] = {chance = 9, id = 'steel', name = 'Steel', quantity = math.random(1,2), limit = 500},
    }
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterServerEvent('supreme_aircon:GiveReward')
+--[[RegisterServerEvent('supreme_aircon:GiveReward')
 AddEventHandler('supreme_aircon:GiveReward', function()
  local source = tonumber(source)
  local item = {}
@@ -41,4 +41,97 @@ end
    end
   end
  end
+end)]]
+
+ESX.RegisterServerCallback('supreme_aircon:PoliceOnline', function(source, cb, pos)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local xPlayers = ESX.GetPlayers()
+	local police = 0
+
+	for i=1, #xPlayers, 1 do
+		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+		if xPlayer.getJob().name == 'police' then
+			police = police + 1
+		end
+	end
+    
+	if (police >= 1) then
+	    cb(true)
+	else
+	    cb(false)
+		if police < 0 then
+			TriggerClientEvent('esx:showNotification', source,'HEY')
+		end
+	end
+end)
+
+RegisterServerEvent('supreme_aircon:GiveReward')
+AddEventHandler('supreme_aircon:GiveReward', function()
+    local source = tonumber(source)
+    local item = {}
+    local gotID = {}
+    local xPlayer = ESX.GetPlayerFromId(source)
+   xPlayer.addInventoryItem('scrapmetal', math.random(12,56))
+end)
+
+RegisterServerEvent('supreme_aircon:GiveReward2')
+AddEventHandler('supreme_aircon:GiveReward2', function()
+ local source = tonumber(source)
+ local item = {}
+ local gotID = {}
+ local xPlayer = ESX.GetPlayerFromId(source)
+xPlayer.addInventoryItem('scrapmetal', math.random(2,11))
+end)
+
+RegisterServerEvent('supreme_aircon:GiveMeterReward')
+AddEventHandler('supreme_aircon:GiveMeterReward', function()
+ local source = tonumber(source)
+ local item = {}
+ local gotID = {}
+ local xPlayer = ESX.GetPlayerFromId(source)
+xPlayer.addMoney(math.random(900,1300))
+end)
+
+RegisterServerEvent('supreme_aircon:GiveMeterReward2')
+AddEventHandler('supreme_aircon:GiveMeterReward2', function()
+ local source = tonumber(source)
+ local item = {}
+ local gotID = {}
+ local xPlayer = ESX.GetPlayerFromId(source)
+xPlayer.addMoney(math.random(40,250))
+end)
+
+RegisterServerEvent('supreme_aircon:GivePayPhoneReward')
+AddEventHandler('supreme_aircon:GivePayPhoneReward', function()
+ local source = tonumber(source)
+ local item = {}
+ local gotID = {}
+ local xPlayer = ESX.GetPlayerFromId(source)
+xPlayer.addMoney(math.random(900,1300))
+end)
+
+RegisterServerEvent('supreme_aircon:GivePayPhoneReward2')
+AddEventHandler('supreme_aircon:GivePayPhoneReward2', function()
+ local source = tonumber(source)
+ local item = {}
+ local gotID = {}
+ local xPlayer = ESX.GetPlayerFromId(source)
+xPlayer.addMoney(math.random(900,1300))
+end)
+
+RegisterServerEvent('supreme_aircon:PickPumpkinReward')
+AddEventHandler('supreme_aircon:PickPumpkinReward', function()
+ local source = tonumber(source)
+ local item = {}
+ local gotID = {}
+ local xPlayer = ESX.GetPlayerFromId(source)
+xPlayer.addInventoryItem('pumpkin', math.random(1,3))
+end)
+
+RegisterServerEvent('supreme_aircon:PickLootBinReward')
+AddEventHandler('supreme_aircon:PickLootBinReward', function()
+    local xPlayer, randomItem2 = ESX.GetPlayerFromId(source), Config.Items2[math.random(1, #Config.Items2)]
+	local xPlayer, randomItem3 = ESX.GetPlayerFromId(source), Config.Items3[math.random(1, #Config.Items3)]
+        xPlayer.addInventoryItem(randomItem2, math.random(1,1))
+		xPlayer.addInventoryItem(randomItem3, math.random(1,2))
 end)
