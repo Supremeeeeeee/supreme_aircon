ca0bab9e5ef92c5bff8f18be3c533faa272b525a
DuncanEll <59232670+Supremeeeeeee@users.noreply.github.com>
2022-03-20 01:06:49 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,5 +5,10 @@ Config.SellScrapLocations = {
     vector3(-448.033, -1701.314, 18.929),
 }
 
+
+--CHANGE THESE ITEMS TO YOUR LIKING
+Config.Items2 = {'phone','goldore','garbage','panties','advanced_lockpick', 'screwdriver', 'money', 'radio', 'drill', 'goldore', 'tinore'}
+Config.Items3 = {'disabler','lockpick','parachute','cutter','bag'}
+ 
 Config.PoliceRequiredToScrap = 0 -- required police to scrap
 Config.CallCopsPercent = 3 -- (min1) if 1 cops will be called every time=100%, 2=50%, 3=33%, 4=25%, 5=20%
